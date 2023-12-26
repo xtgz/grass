4804bfe942d91a8a1d639dab95a9f92aac853459
necklace <xiangxn@163.com>
2023-12-26 18:29:08 +0800

    fix bugs

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -s http://myip.ipip.net
+export GLOBAL_AGENT_HTTP_PROXY=http://172.17.0.1:1087
 /root/.nvm/versions/node/v19.9.0/bin/node -r 'global-agent/bootstrap' --max-http-header-size=1073741824 ./src/app.js
