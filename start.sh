9e50c66477a133d1a9c711d982c78dc632b367b1
necklace <xiangxn@163.com>
2023-12-26 18:09:46 +0800

    fix bug

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -s http://myip.ipip.net
-/root/.nvm/versions/node/v19.9.0/bin/node --max-http-header-size=1073741824 ./src/app.js
+/root/.nvm/versions/node/v19.9.0/bin/node -r 'global-agent/bootstrap' --max-http-header-size=1073741824 ./src/app.js
